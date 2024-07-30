72798278a9b2f41bc12c9808aecec069a99ec15b
masonzoyt <m@zoyt.net>
2024-07-30 09:18:18 -0700

    i think i added the button at this point, along with scripts.js and 1.html but not sure

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Get the button element by its ID
+const button = document.getElementById('imhere');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    // Open a new page
+    window.location.href = '1.html'; // Replace with the desired URL
+});
